b3704a1104eb31c4a124f2d4317a835ffe3b6e4b
Sagar Mangulkar <smangul1@mac-w89502dr5pk.pods.bu.int>
2017-01-21 20:08:17 -0500

    Initial Commit

---
+++ FirstAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstAssignment
+//
+//  Created by Sagar Mangulkar on 1/21/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstAssignment
+//
+//  Created by Sagar Mangulkar on 1/21/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
